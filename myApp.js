2fccd429a2271aa2560bb0348b8014fbbe96feff
okumito <126346137+okumito@users.noreply.github.com>
2025-10-04 09:40:43 -0400

    git add git commit -m ejercicio 1 git push origin practice

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
 
+app.get("/", function(req, res) {
+    res,send('hello World')
+});
 
-
-
+app.listen(3000, function() {
+    console.log('servidor iniciado en puerto 3000')
+});
 
 
 
